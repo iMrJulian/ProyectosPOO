7a585184a0f928857d007f951a2ad915cafaf1a7
iMrJulian <juliandrincon@gmail.com>
2018-02-10 19:10:56 -0500

    Initial commit

---
+++ aypdeuncuadrado/src/aypdeuncuadrado/Aypdeuncuadrado.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aypdeuncuadrado;
+import java.util.Scanner;
+/**
+ *
+ * @author david
+ */
+public class Aypdeuncuadrado {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     int l,p,a;
+     Scanner sc = new Scanner(System.in);
+     System.out.println("Digite el lado del cuadro:");
+     l = sc.nextInt();
+     p=l*4;
+     System.out.println("El perimetro del cuadrado es: "+p);
+     
+        // TODO code application logic here
+    }
+    
+}
